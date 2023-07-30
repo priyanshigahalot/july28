79732d4cb91e4cc9000b86b81cd4adf1c63ba235
priyanshi <priyanshigahalot@gmail.com>
2023-07-31 00:11:20 +0530

    array

---
+++ traversal.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    //traversal code
+    printf("the elements are- ");
+ int arra[5]={4,5,6,3,2};
+ for(int i=0;i<5;i++){
+
+printf("%d\n",arra[i]);
+ }
+
+return 0;
+}
